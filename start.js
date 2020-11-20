2f8ef91b96990dd6ad0fd27aa9473a636b89e252
david espinal <despinal0425@gmail.com>
2020-11-20 12:01:31 -0500

    watched video and went over the code-along

--- start.js
+++ start.js
@@ -1,51 +1,173 @@
+//video time: 27.00
+//Axios http library or http client to make request either to your own backend or 
+//third party API to fetch data
+
+//AXIOS GLOBALS 
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  // axios({
+  //   method: 'get',
+  //   url: 'https://jsonplaceholder.typicode.com/todos',
+  //   params: { //and url parameter you wanna add. shorten list from 200 to 5
+  //     _limit:5
+  //   }
+  // }) //will give us a promsie
+  //   .then(response => showOutput(response)) //this is where we will put our response
+  //   .catch(error => console.log(error(error))) //this handles error from promises
+
+  //---------below is a short version of how you can  shorten the list 200 to 5
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=5') //will give us a promsie
+    .then(response => showOutput(response)) //this is where we will put our response
+    .catch(err => console.error(err)) //this handles error from promises
 }
 
 // POST REQUEST
-function addTodo() {
-  console.log('POST Request');
+function addTodo() { //sending data to server or API. This increase list from 200 to 201
+  axios.post('https://jsonplaceholder.typicode.com/todos', {
+    title: 'New Todo',
+    completed: false
+  }) //will give us a promsie
+    .then(response => showOutput(response)) //this is where we will put our response
+    .catch(err => console.log(error(err))) //this handles error from promises
 }
 
-// PUT/PATCH REQUEST
+// PUT/PATCH REQUEST //'Put' is meant to replace the entire resource(or selected object) & 'Patch' is meant to update and increment the resource(it grabs the specific object, then grabs the specific key to change value)
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios
+    .patch('https://jsonplaceholder.typicode.com/todos/1', {
+      title: 'Updated Todo',
+      completed: true
+  }) //will give us a promsie
+    .then(response => showOutput(response)) //this is where we will put our response
+    .catch(err => console.log(error(err))) //this handles error from promises
 }
 
-// DELETE REQUEST
+// DELETE REQUEST //Deletes any object or data from the api
 function removeTodo() {
-  console.log('DELETE Request');
+  axios
+    .delete('https://jsonplaceholder.typicode.com/todos/1') //will give us a promsie
+    .then(response => showOutput(response)) //this is where we will put our response
+    .catch(err => console.log(error(err))) //this handles error from promises
 }
 
-// SIMULTANEOUS DATA
+// SIMULTANEOUS DATA //making simultaneuos request. Lets get post data, and todo
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([ //all will allow to do an array of requests
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+    .then(axios.spread((todos, posts) => showOutput(posts))) //axios spread gives variable like post and todos cleaner. The variable represents the array response that contains todos, and posts
+    .catch(err => console.log(error(err)))
+    
 }
 
-// CUSTOM HEADERS
-function customHeaders() {
-  console.log('Custom Headers');
+// CUSTOM HEADERS // when you have authentication, you login to api, and get token--send token to the header to get access to protective route
+function customHeaders() { //login to create a Post request
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'sometoken'
+    }
+  }
+
+  axios.post('https://jsonplaceholder.typicode.com/todos', {//request
+    title: 'New Todo', 
+    completed: false
+  }, config) //Add config as a third parameter to costum header   //will give us a promise
+    .then(response => showOutput(response)) //this is where we will put our response
 }
 
-// TRANSFORMING REQUESTS & RESPONSES
+// TRANSFORMING REQUESTS & RESPONSES 
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method: 'post',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    data: {
+      title: 'Hello World'
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data => {
+      data.title = data.title.toUpperCase();
+      return data;
+    }) //take default transformReponse and overwrite it
+  };
+
+  axios(options).then(response => showOutput(response));//axios by default will do a get request and change the get request to whatever we put on option
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios
+    .get('https://jsonplaceholder.typicode.com/todoss') //will give us a promsie
+    .then(response => showOutput(response)) //this is where we will put our response
+    .catch(err => {
+      if (err.response) { //add a condition to check for any error response (404 page)
+        //if true, server responded with a status other than 200 range
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+
+        if (err.response.status === 404) {
+          alert("Error: Page Not Found");
+        }
+      } else if (err.request) {
+        //Request was made but there was no response
+        console.error(err.request);
+      } else {
+        console.error(err.message);
+      }
+    }); //this handles error from promises
 }
 
-// CANCEL TOKEN
+// CANCEL TOKEN we can cancel request on the fly
 function cancelToken() {
-  console.log('Cancel Token');
+  const source = axios.CancelToken.source();
+  axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+      cancelToken: source.token
+    }) //will give us a promsie
+    .then(response => showOutput(response))
+    .catch(thrown => {
+      if (axios.isCancel(thrown)) {
+        console.log('Request canceled', thrown.message)
+      }
+    });
+  
+  if (true) {
+    source.cancel("Request canceled!")
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+//interceptors allow us to intercept requests and run some kind of functionality
+
+axios.interceptors.request.use(//config give us access to method, url, etc
+  config => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 // AXIOS INSTANCES
+const axiosInstance = axios.create({
+  //Other custom settings
+  baseURL:'https://jsonplaceholder.typicode.com'
+});
+
+// axiosInstance.get('/comments').then(response => showOutput(response));
+
+
 
 // Show output in browser
 function showOutput(res) {
